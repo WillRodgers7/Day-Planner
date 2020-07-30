635bdb2cceca52a8ccc052c00616837d088753c7
Will Rodgers <WillRodgers.e.s@gmail.com>
2020-07-29 22:13:11 -0700

    new update

--- Develop/script.js
+++ Develop/script.js
@@ -5,3 +5,11 @@ dt = DateTime.fromObject({
     zone: 'America/Los_Angeles',
     numberingSystem: 'beng'
 })
+
+
+moment('2016-03-12 13:00:00').add(24, 'hours').format('LLL')
+"March 13, 2016 2:00 PM"
+
+$("#timer").text(moment + add + format)
+
+// $("#timer").text(dt.day + "" + dt.hour + "" + dt.zone)
