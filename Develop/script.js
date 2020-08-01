d1a6b9c821e78c39646999cf82ef76871d032584
Will Rodgers <WillRodgers.e.s@gmail.com>
2020-07-31 17:42:53 -0700

    update

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,11 @@
 var DateTime = luxon.DateTime;
 dt = DateTime.fromObject({
-    day: 22,
-    hour: 12,
+    day: 29,
+    hour: 11,
     zone: 'America/Los_Angeles',
     numberingSystem: 'beng'
 })
 
 
-moment('2016-03-12 13:00:00').add(24, 'hours').format('LLL')
-"March 13, 2016 2:00 PM"
 
-$("#timer").text(moment + add + format)
-
-// $("#timer").text(dt.day + "" + dt.hour + "" + dt.zone)
+$("#timer").text(moment().format("MMMM DD, YYYY hh:mm A"))
